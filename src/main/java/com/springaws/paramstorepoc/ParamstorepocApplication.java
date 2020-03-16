777e5e0c59bf6a49ee2d6265c28bceaabfbcd694
rohansurve rohansurve <rohansurve26@gmail.com>
2020-03-16 10:36:25 -0500

    adding testing rest

--- src/main/java/com/springaws/paramstorepoc/ParamstorepocApplication.java
+++ src/main/java/com/springaws/paramstorepoc/ParamstorepocApplication.java
@@ -7,11 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParamstorepocApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("aws.accessKeyId","AKIAJNCJOGNRGGZP7X5A");
-        System.setProperty("aws.secretKey","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");
+		System.setProperty("aws.accessKeyId","");
+        System.setProperty("aws.secretKey","");
         System.setProperty("aws.region","us-east-2");
 		
-       //.setProperty("cloud.aws.credentials.access-key","bNrz+BANm8CQe2PqVGHZC2aPvoZnaMTchMn43P6A");
 
 		SpringApplication.run(ParamstorepocApplication.class, args);
 	}
